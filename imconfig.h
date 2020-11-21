9c5ae5f65285242beaa9c49634acc522de16ed44
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2020-11-21 14:32:31 +0100

    restore config

--- imconfig.h
+++ imconfig.h
@@ -66,7 +66,8 @@
 //---- Define constructor and implicit cast operators to convert back<>forth between your math types and ImVec2/ImVec4.
 // This will be inlined as part of ImVec2 and ImVec4 class declarations.
 
-#define IM_VEC2_CLASS_EXTRA                                                 \
+#define IM_VEC2_CLASS_EXTRA \
+        ImVec2( const float _value){ x = y = _value;} \
         ImVec2 operator+(ImVec2 _v) { return ImVec2(x + _v.x,y + _v.y); }
 
 /*
